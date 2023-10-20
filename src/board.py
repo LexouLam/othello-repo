c5c1b8da5d90eefcfd451b789f316b9da4357a27
Alexandra Lamotte <alexandra.lamotte@gmail.com>
2023-10-20 14:31:37 +0200

    add letters and numbers

--- src/board.py
+++ src/board.py
@@ -55,19 +55,21 @@ class Board:
         return color, good_neighbors
 
     def draw(self):
-        for col in range(8):
-            print(" ", "-" * 31, sep="")
-            print("| ", end="")
-            for row in range(8):
-                if self.array[col][row] == pawn.Pawn.EMPTY:
+        print("    A  ", " B  ", " C  ", " D  ", " E  ", " F  ", " G  ", " H  ", sep="")
+        for row in range(8):
+            print("  ", "-" * 33, sep="")
+            print(row + 1, "| ", end="")
+            for col in range(8):
+                if self.array[row][col] == pawn.Pawn.EMPTY:
                     print("  | ", end="")
-                elif self.array[col][row] == pawn.Pawn.WHITE:
+                elif self.array[row][col] == pawn.Pawn.WHITE:
                     print("O | ", end="")
-                elif self.array[col][row] == pawn.Pawn.BLACK:
+                elif self.array[row][col] == pawn.Pawn.BLACK:
                     print("X | ", end="")
                 else:
                     print("? | ", end="")
-            print(" ", " " * 31, sep="")
+            print("  ", " " * 33, sep="")
+        print("  ", "-" * 33, sep="")
 
 
 if __name__ == "__main__":
