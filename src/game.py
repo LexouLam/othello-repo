aac56490027671ceb9d2bbde9660dea101d0bed9
nparent <nicolas.parent.msiam@gmail.com>
2023-10-23 13:45:10 +0200

    game corresponds to the graph

--- src/game.py
+++ src/game.py
@@ -13,38 +13,35 @@ class Game:
         self.player2 = player2
         self.playing_player = self.player1
         self.board = board.Board()
-        self.tour_counter = 0
+        self.pawn_counter = 0
 
     def start(self):
-        player = self.player1
-        while self.game_can_continue():
-            if not player.player_can_play(self.board):
-                player = self.change_player(player)
-            input_is_wrong = True
-            while input_is_wrong:
-                self.board.draw()
-                row, col = map(
-                    int, input(f"Enter row and col ({player.color}):  ").split()
-                )
-                position = pawn.Position(row, col)
-                if not position.valid_position():
-                    print("wrong input, try again")
+        player = self.player1  # player1 has white pawns so he starts
+        flag_one_cant_play = False
+        while True:
+            if not self.pawn_left():  # all pawns are used so end of games
+                return self.end_of_game()
+            else:
+                if not player.player_can_play(self.board):
+                    if flag_one_cant_play:
+                        return self.end_of_game()
+                    else:
+                        player = self.change_player(player)
+                        flag_one_cant_play = True
                     continue
-                list_of_changes = self.board.check_can_play(player.color, position)
-                if list_of_changes:
-                    self.board.apply_list_of_changes(list_of_changes)
-                    input_is_wrong = False
-                    player = self.change_player(player)
-                else:
-                    print("wrong input, try again")
-
+                else:  # player can play
+                    flag_one_cant_play = False
+                    position = self.read_input(player)
+                    list_of_changes = self.board.check_can_play(player.color, position)
+                    if (
+                        list_of_changes
+                    ):  # the pawn color and position implies some pawn flip
+                        self.board.apply_list_of_changes(list_of_changes)
+                        player = self.change_player(player)
+                        self.pawn_counter += 1
+                    else:
+                        print("wrong input, try again")
         pass
-
-    def game_can_continue(self) -> bool:
-        return (
-            self.player1.player_can_play(self.board)
-            or self.player2.player_can_play(self.board)
-        ) and self.tour_counter < 60
 
     def change_player(self, player: player.Player):
         if player == self.player1:
@@ -52,11 +49,30 @@ class Game:
         else:
             return self.player1
 
+    def pawn_left(self):
+        return self.pawn_counter != 60
+
+    def end_of_game(self):
+        # compute score
+        # prompt winner
+        pass
+
+    def read_input(self, player):
+        input_is_wrong = True
+        while input_is_wrong:
+            self.board.draw()
+            row, col = map(int, input(f"Enter row and col ({player.color}):  ").split())
+            position = pawn.Position(row, col)
+            if not position.valid_position():
+                print("wrong input, try again")
+                continue
+            else:
+                input_is_wrong = False
+        return position
+
 
 if __name__ == "__main__":
     nicolas = player.Player("Nicolas", pawn.Pawn.WHITE)
     alexandra = player.Player("Alexandra", pawn.Pawn.BLACK)
     game = Game(nicolas, alexandra)
-    # for i, j, element in game.board:
-    #     print(i, j, element)
     game.start()
